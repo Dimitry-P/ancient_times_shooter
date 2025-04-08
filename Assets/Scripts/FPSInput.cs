6c60afb8f1c79d30fa2cb9e2577ae68779eaba85
Dimitry-P <dimitry.piatnitsky@yahoo.com>
2025-04-08 19:30:39 +0300

    JumpAbilityForFPS

--- Assets/Scripts/FPSInput.cs
+++ Assets/Scripts/FPSInput.cs
@@ -1,73 +1,77 @@
 using UnityEngine;
-using System.Collections;
-using System.IO;
-using UnityEngine.InputSystem.XR;
 
 [RequireComponent(typeof(CharacterController))]
 [AddComponentMenu("Control Script/FPS Input")]
-
-
 public class FPSInput : MonoBehaviour
 {
     private CharacterController _charController;
+
+    [Header("Movement Settings")]
     public float speed = 6.0f;
-    public float gravity = -9.8f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 2f;
+
+    [Header("Mouse Settings")]
     public float sensitivityHor = 3.5f;
 
-    #region Jump
+    [Header("Ground Check")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundDistance = 0.3f;
+    [SerializeField] private LayerMask groundMask;
+
+    [Header("Inventory UI")]
+    [SerializeField] private GameObject inventoryPanel;
+    private bool inventoryPanel_isActive = false;
+
     private Vector3 velocity;
     private bool isGrounded;
-    public float jumpHeight = 2f;
-    #endregion
-
-    [SerializeField] private GameObject inventoryPanel;
-    bool inventoryPanel_isActive = false;
 
     void Start()
     {
+        _charController = GetComponent<CharacterController>();
+
         if (GameController.instance != null && GameController.instance.settingsManager != null && GameController.instance.settingsManager.ControlDTO != null)
         {
             sensitivityHor = GameController.instance.settingsManager.ControlDTO.mouseSens;
         }
-        else
-        {
-            Debug.LogWarning("GameController или его компоненты не инициализированы.");
-            sensitivityHor = 3.5f;
-        }
-
-        _charController = GetComponent<CharacterController>();
     }
 
     void Update()
     {
+        // Проверяем, на земле ли игрок
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        // Обнуляем вертикальную скорость при касании земли
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        // Вращение по горизонтали
         transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
 
-        float deltaX = Input.GetAxis("Horizontal") * speed;
-        float deltaZ = Input.GetAxis("Vertical") * speed;
-        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
-        movement = Vector3.ClampMagnitude(movement, speed);
-        movement.y += gravity;
-        movement *= Time.deltaTime;
-        movement = transform.TransformDirection(movement);
-        _charController.Move(movement);
+        // Движение по горизонтали и вперёд/назад
+        float deltaX = Input.GetAxis("Horizontal");
+        float deltaZ = Input.GetAxis("Vertical");
 
+        Vector3 move = transform.right * deltaX + transform.forward * deltaZ;
+        _charController.Move(move * speed * Time.deltaTime);
+
+        // Прыжок
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // Применяем гравитацию
+        velocity.y += gravity * Time.deltaTime;
+        _charController.Move(velocity * Time.deltaTime);
+
+        // Открытие/закрытие инвентаря
         if (Input.GetKeyDown(KeyCode.I))
         {
             inventoryPanel_isActive = !inventoryPanel_isActive;
             inventoryPanel.SetActive(inventoryPanel_isActive);
-
         }
-
-    }
-
-    private void ApplyGravity()
-    {
-        if (isGrounded && velocity.y < 0)
-            velocity.y = -2f; // Сбросить вертикальную скорость при приземлении
-
-        velocity.y += gravity * Time.deltaTime;
-
-        // Перемещение контроллера с учетом вертикальной скорости
-        _charController.Move(velocity * Time.deltaTime);
     }
 }
