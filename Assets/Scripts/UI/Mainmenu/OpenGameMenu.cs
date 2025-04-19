34116f5d2f1c077d1e8efd7ee61593b9df066ec5
PavlenkoIGOR <illusiopard@gmail.com>
2025-04-19 12:48:17 +0300

    gameMenuStartDevelope

--- Assets/Scripts/UI/Mainmenu/OpenGameMenu.cs
+++ Assets/Scripts/UI/Mainmenu/OpenGameMenu.cs
@@ -2,21 +2,24 @@ using UnityEngine;
 
 public class OpenGameMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject _gameMenuPanel;
+    [SerializeField] private FPSInput _fpsInput;
     bool isMainMenuOpened;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.gameObject.SetActive(false);
+        _gameMenuPanel.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("esc update");
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             isMainMenuOpened = !isMainMenuOpened;
-            transform.gameObject.SetActive(isMainMenuOpened);
+            _gameMenuPanel.gameObject.SetActive(isMainMenuOpened);
+            Time.timeScale = isMainMenuOpened == true ? 0 : 1;
+            _fpsInput.gameObject.SetActive(!isMainMenuOpened);
             Debug.Log("esc");
         }
     }
